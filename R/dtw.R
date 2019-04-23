5506265be320ba33e1710bca9add0b774d27e7ca
lauriekell <laurie@kell.es>
2019-04-23 11:06:59 +0100

    tidy

--- R/dtw.R
+++ R/dtw.R
@@ -1,6 +1,8 @@
+if (FALSE}){
 library("dtw")
 data("aami3a")
 ref <-ts(subset(dgs,factor=="base"&level=="wg"&name=="Age-4")$obs)
 test<-ts(subset(dgs,factor=="base"&level=="wg"&name=="Age-5+")$obs)
 
 plot(dtw(test,ref,k=TRUE),type="two",off=1,match.lty=2,match.indices=20)
+}
